d694c83946a46740418e1efaf0417d8a0ed11ff4
ttechgirl <abiodunayisat2@gmail.com>
2024-09-01 23:08:36 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Hello, Aisha!");
+Console.Write("Hello, seyi!");
+DateTime time = DateTime.Now.ToLocalTime();
+
+Console.WriteLine("Current local time: " + time);
+PureCsharp pureCsharp = new PureCsharp();
+pureCsharp.Generator();
+
+
+class  PureCsharp
+{
+    public void Generator()
+    {
+        Console.WriteLine("Enter password");
+        Console.WriteLine("=================");
+        Console.WriteLine("Password must be 6 characters long");
+        Console.WriteLine("=================");
+
+        var password = Console.ReadLine();
+
+        if(password != null && password.Length >= 6)
+        {
+            var guid = Guid.NewGuid().ToString("N")[2..6];
+            
+            var generatedPassword = password + "" + guid;
+            Console.WriteLine($"Suggested password is {generatedPassword}");
+            return ;
+        }
+        else
+        {
+            Console.WriteLine("=================");
+            Console.WriteLine("Wrong password format");
+            return;
+        }
+    }
+    
+    
+
+}
